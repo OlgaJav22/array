0d5523f0923ef32850e62518e0277c2b52f00505
OlgaJav22 <olga.ko4erga@yandex.ru>
2022-10-12 22:50:54 +0300

    часть д/з сделана

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
 public class Main {
     public static void main(String[] args) {
 
-        int [] w
-            
+        // Задачав 1.1
+        int [] array = new int [] {1, 2, 3};
+
+        // Задача 1.2
+
+        double [] box = {1.57, 7.654, 9.986};
+
+
+        // Задача 1.3
+
+        int [] i = new int [] {1, 3, 5, 7, 9, 11};
+
+       //Задача 2
+
+        System.out.println(array [0] + ", " + array[1] + ", " + array[2]);
+        System.out.println(box [0] + ", " + box[1] + ", " + box [2]);
+        System.out.println(i [0] + ", " + i[1] + ", " + i [2] + ", " + i[3] + ", " + i [4] + ", " + i[5]);
+
+        //Задача 3
+
+        System.out.println(array [2] + ", " + array[1] + ", " + array[0]);
+        System.out.println(box [2] + ", " + box[1] + ", " + box [0]);
+        System.out.println(i [5] + ", " + i[4] + ", " + i [3] + ", " + i[2] + ", " + i [1] + ", " + i[0]);
+
         }
     }
